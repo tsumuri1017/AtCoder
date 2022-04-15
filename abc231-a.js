56b88001d644f80e8b53fa48b0dc2353e4a00b6e
つむり <tsumuri2008@gmail.com>
2022-04-15 21:32:20 +0900

    Create abc231-a.js

---
+++ abc231-a.js
@@ -0,0 +1,10 @@
+/*
+https://atcoder.jp/contests/abc231/submissions/27833661
+*/
+
+function main(input) {
+  var a = input.split(" ");
+ a=a/100;
+  console.log(a);
+}
+main(require("fs").readFileSync("/dev/stdin", "utf8"));
