61dcfbc3ae037f62e3cbb2cfa2c8b51c2a60c25d
つむり <tsumuri2008@gmail.com>
2022-04-17 09:13:39 +0900

    Create abc247-a.js

---
+++ abc247-a.js
@@ -0,0 +1,12 @@
+/*
+https://atcoder.jp/contests/abc247/submissions/30851797
+*/
+function Main(input) {
+input = input.split("");
+a=String(input[0]);
+b=String(input[1]);
+c=String(input[2]);
+r='0'+a+b+c;
+console.log(r)
+}
+Main(require("fs").readFileSync("/dev/stdin", "utf8"));
